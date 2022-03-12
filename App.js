09517c4f99fa8a485e8cab78e097e418b5b519f2
Joha Kim <johakim9936@gmail.com>
2022-03-12 18:57:32 -0500

    change main screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StyleSheet, View } from "react-native";
 import SplitView from "./app/screens/SplitView";
 import Home from "./app/screens/Home";
-import Login from "./app/screens/Login"
+import Login from "./app/screens/Login";
 import CreateEvent from "./app/screens/CreateEvent";
 
 export default function App() {
@@ -9,7 +9,7 @@ export default function App() {
     <View style={styles.container}>
       {/* <Home /> */}
       {/* <SplitView /> */}
-      <Login />
+      {/* <Login /> */}
       <CreateEvent />
     </View>
   );
