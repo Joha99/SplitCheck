67fbe6d3b5b475e1446f8c69103926bb8d14450d
Steven Chen <schen2425@gmail.com>
2022-04-03 18:30:48 -0400

    added more navigation

--- app/navigation/CreateEventNavigator.js
+++ app/navigation/CreateEventNavigator.js
@@ -3,11 +3,17 @@ import { createStackNavigator } from "@react-navigation/stack";
 import CreateEvent from "../screens/CreateEvent";
 import PaymentDivideOption from "../screens/PaymentDivideOption";
 import EqualPayment from "../screens/EqualPayment";
+import Home from "../screens/Home";
+import SettlePayment from "../screens/SettlePayment";
+import History from "../screens/History";
 
 const Stack = createStackNavigator();
 
 const CreateEventNavigator = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Home" component={Home} />
+    <Stack.Screen name="SettleUp" component={SettlePayment} />
+    <Stack.Screen name="History" component={History} />
     <Stack.Screen name="CreateEvent" component={CreateEvent} />
     <Stack.Screen name="PaymentDivideOption" component={PaymentDivideOption} />
     <Stack.Screen name="EqualPayment" component={EqualPayment} />
