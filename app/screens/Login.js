a14dee5e725d45a2518ce778df2c07deb9d36ff8
Anshul Agrawal <anshul.jkt@gmail.com>
2022-04-08 19:31:26 -0400

    phone login now successfully works and takes you to the home page

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,72 +1,35 @@
 import React, { Component, useEffect, useState, useRef } from 'react';
-import { Alert, TextInput, View, StyleSheet } from 'react-native';
+import { Alert, TextInput, View, StyleSheet, ActivityIndicator, Toast } from 'react-native';
 import { Header } from 'react-native/Libraries/NewAppScreen';
-import { auth } from "../../firebase";
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword, RecaptchaVerifier, sendPasswordResetEmail } from 'firebase/auth';
+import { auth, app } from "../../firebase";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, RecaptchaVerifier, PhoneAuthProvider, sendPasswordResetEmail, signInWithCredential } from 'firebase/auth';
+import { FirebaseRecaptchaVerifierModal, FirebaseRecaptchaBanner } from 'expo-firebase-recaptcha';
 import { useAuthState } from "react-firebase-hooks/auth";
 import Screen from "../components/Screen";
 import defaultStyles from "../config/styles";
 import AppText from "../components/AppText";
 import AppTextInput from "../components/AppTextInput";
 import Button from "../components/Button";
+import colors from "../config/colors";
+// import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input/react-native-input'
 
 export default function Login({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phoneNum, setPhone] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
   const [user, loading, error] = useAuthState(auth);
-  const appVerifier = window.recaptchaVerifier;
-  
-  // const recaptchaVerifier = new auth.RecaptchaVerifier('recaptcha-container', {}, auth);
 
-  function createUser(email, password) {
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      user = userCredential.user;
-      console.log(user.email)
-      console.log(user.password)
-      navigation.navigate("Home");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage)
-    });                
-  }
-  function signInUser(email, password) {
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      user = userCredential.user;
-      console.log(user.email)
-      console.log(user.password)
-      navigation.navigate("Home");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage)
-    });                
-  }
+  const recaptchaVerifier = React.useRef(null);
 
-  function sendPasswordReset(email) {
-    sendPasswordResetEmail(auth, email)
-    .then((response) => {
-      // Signed in 
-      console.log(response)
-      navigation.navigate("Home");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage)
-    });                
-  }
-  
-  function setUpRecaptcha() {
-    
-  }
+  const FIREBASE_CONFIG = app ? app.options : undefined;
+  const [phoneNumber, setPhoneNumber] = React.useState('');
+  const [verificationId, setVerificationId] = React.useState('');
+  const [verifyError, setVerifyError] = React.useState();
+  const [verifyInProgress, setVerifyInProgress] = React.useState(false);
+  const [verificationCode, setVerificationCode] = React.useState('');
+  const [confirmError, setConfirmError] = React.useState();
+  const [confirmInProgress, setConfirmInProgress] = React.useState(false);
+  const isConfigValid = !!FIREBASE_CONFIG.apiKey;
 
   useEffect(() => {
     if (loading) {
@@ -75,8 +38,15 @@ export default function Login({ navigation }) {
     }
     if (user) {
       navigation.navigate("Home")
-      console.log(user.email + " is already logged in!")
+      console.log(user.displayName + " is already logged in!")
+      Alert.alert(
+        `Welcome back, ${user.displayName}!`,
+        "You are already logged in.",
+        [{ text: "OK"}]
+      );
       console.log(user.phoneNumber)
+      console.log(user.email)
+      console.log(user.uid)
     };
   }, [user, loading]);
   
@@ -85,63 +55,106 @@ export default function Login({ navigation }) {
       <View style={[defaultStyles.centerItems, styles.titleContainer]}>
         <AppText style={defaultStyles.title}>Log In</AppText>
       </View>
+      <FirebaseRecaptchaVerifierModal
+        ref={recaptchaVerifier}
+        firebaseConfig = {FIREBASE_CONFIG}
+        attemptInvisibleVerification={true}
+      />
       <View style={styles.inputContainer}>
-        <View style={styles.input}>
-          <AppText>Email Address</AppText>
-          <AppTextInput 
-              placeholder="money@bags.com" 
-              blurOnSubmit={true} 
-              // keyboardType={'phone-pad'}
-              onChangeText={text => setEmail(text)}/>
+      <View style={styles.input}>
+          <AppText>Phone Number</AppText>
+          <PhoneInput
+          style={
+            [{backgroundColor: colors.light,
+              borderRadius: 5,
+              flexDirection: "row",
+              padding: 15,
+              marginVertical: 10}]
+          }
+            defaultCountry='US'
+            placeholder="+1 404-123-6789"
+            blurOnSubmit = {true} 
+            value={phoneNumber}
+            // autoFocus={isConfigValid}
+            onChange={setPhoneNumber}/>
         </View>
+        <Button
+          style={[
+            { backgroundColor: defaultStyles.colors.secondary },
+            styles.input,
+          ]}
+          disabled={!phoneNumber}
+          onPress={async () => {
+            const phoneProvider = new PhoneAuthProvider(auth);
+            try {
+              setVerifyError(undefined);
+              setVerifyInProgress(true);
+              setVerificationId('');
+              const verificationId = await phoneProvider.verifyPhoneNumber(
+                phoneNumber,
+                // @ts-ignore
+                recaptchaVerifier.current
+              );
+              setVerifyInProgress(false);
+              setVerificationId(verificationId);
 
+            } catch (err) {
+              setVerifyError(err);
+              setVerifyInProgress(false);
+            }
+          }}
+        >
+          <AppText>{verificationId ? 'Resend' : 'Send'} Verification Code</AppText>
+        </Button>
+          {verifyError && <AppText style={styles.error}>{`Error: ${verifyError.message}`}</AppText>}
+          {verifyInProgress && <ActivityIndicator style={styles.loader} />}
+          {verificationId ? (<AppText style={styles.success}>A verification code has been sent to your phone!</AppText>): ( undefined )}
         <View style={styles.input}>
-          <AppText>Password</AppText>
+          <AppText>Enter Verification Code:</AppText>
           <AppTextInput 
-                      placeholder="MakeMeSecure9!?" 
+                      placeholder="123456" 
                       secureTextEntry={true} 
-                      editable={true} 
-                      onChangeText={text => setPassword(text)}
+                      editable={!!verificationId}
+                      onChangeText={text => setVerificationCode(text)}
                       blurOnSubmit={true}/>
         </View>
-
-<Button
-          style={[
-            { backgroundColor: defaultStyles.colors.secondary },
-            styles.input,
-          ]}
-          onPress={() => {
-            signInUser(email, password)
-          }}
-        >
-          <AppText>Sign In</AppText>
-        </Button>
-
         <Button
-          style={[
-            { backgroundColor: defaultStyles.colors.secondary },
-            styles.input,
-          ]}
-          onPress={() => {
-            createUser(email, password)
-          }}
-        >
-          <AppText>Sign Up</AppText>
-        </Button>
-
-        <Button
-        style={[
-          {backgroundColor: defaultStyles.colors.secondary},
-          styles.input,
-        ]}
-        onPress={() => {
-          sendPasswordReset(email)
-        }}
-        >
-          <AppText>Reset Password</AppText>
+            style={[
+              { backgroundColor: defaultStyles.colors.secondary },
+              styles.input,
+            ]}
+            disabled={!verificationCode}
+            onPress={async () => {
+              try {
+                setConfirmError(undefined);
+                setConfirmInProgress(true);
+                const credential = PhoneAuthProvider.credential(
+                  verificationId,
+                  verificationCode
+                );
+                const authResult = await signInWithCredential(auth, credential);
+                setConfirmInProgress(false);
+                setVerificationId('');
+                setVerificationCode('');
+              } catch (err) {
+                setConfirmError(err);
+                setConfirmInProgress(false);
+              }
+            }}
+          >
+            <AppText>Confirm Verification Code</AppText>
           </Button>
-      </View>
-    </Screen>
+          {confirmError && <AppText style={styles.error}>{`Error: ${confirmError.message}`}</AppText>}
+          {confirmInProgress && <ActivityIndicator style={styles.loader} />}
+        </View>
+        {!isConfigValid && (
+          <View style={styles.overlay} pointerEvents="none">
+            <AppText style={styles.overlayText}>
+              To get started, set a valid FIREBASE_CONFIG in App.tsx.
+            </AppText>
+          </View>
+        )}
+      </Screen>
   );
 }
 
@@ -161,5 +174,18 @@ const styles = StyleSheet.create({
   },
   input: {
     marginBottom: 15,
+  },
+  error: {
+    marginTop: 10,
+    fontWeight: 'bold',
+    color: colors.danger,
+  },
+  success: {
+    marginBottom: 20,
+    fontWeight: 'bold',
+    color: colors.secondary,
+  },
+  loader: {
+    marginTop: 10,
   },
 });
