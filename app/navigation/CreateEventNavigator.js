88e2be2f8b3f26acabe80646019fe76066e9260f
Anshul Agrawal <anshul.jkt@gmail.com>
2022-04-13 12:41:13 -0400

    Update CreateEventNavigator.js
    removed login and home screen from createeventnavigator

--- app/navigation/CreateEventNavigator.js
+++ app/navigation/CreateEventNavigator.js
@@ -3,7 +3,6 @@ import { createStackNavigator } from "@react-navigation/stack";
 import CreateEvent from "../screens/CreateEvent";
 import PaymentDivideOption from "../screens/PaymentDivideOption";
 import EqualPayment from "../screens/EqualPayment";
-import Login from "../screens/Login";
 import Home from "../screens/Home";
 import SettlePayment from "../screens/SettlePayment";
 import History from "../screens/History";
@@ -12,8 +11,6 @@ const Stack = createStackNavigator();
 
 const CreateEventNavigator = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="Login" component={Login} />
-    <Stack.Screen name="Home" component={Home} />
     <Stack.Screen name="CreateEvent" component={CreateEvent} />
     <Stack.Screen name="PaymentDivideOption" component={PaymentDivideOption} />
     <Stack.Screen name="EqualPayment" component={EqualPayment} />
