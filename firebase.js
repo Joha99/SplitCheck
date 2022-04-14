8dd74f415efced1f9cfbb283ac84bc56a68f849a
Anshul Agrawal <anshul.jkt@gmail.com>
2022-04-14 00:34:52 -0400

    fixed persistence of auth by cahnging import

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import Constants from 'expo-constants';
 import { getAuth, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { AsyncStorage } from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getReactNativePersistence } from 'firebase/auth/react-native'
 
 const firebaseConfig = {
